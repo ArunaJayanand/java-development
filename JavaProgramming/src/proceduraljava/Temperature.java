6e5d87e69092c6911e61a269b63e558c68f319cd
ARUNA JAYANAND <aruna.k.jayanand@gmail.com>
2019-11-29 10:57:18 -0500

    Checking in Temperature.java

---
+++ JavaProgramming/src/proceduraljava/Temperature.java
@@ -0,0 +1,35 @@
+
+package proceduraljava;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * This class is to convert temperature in celcius to farenheit and categorize the wrather
+ * as COLD WAETHER, PRETTY WARM or NORMAL.
+ * 
+ * @author Aruna
+ *
+ */
+class Temperature {
+	
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the temperature in celcious: ");
+		float tempCel = sc.nextFloat();
+		sc.close();
+		float tempInFaren = tempCel * (9f / 5) + 32;
+		System.out.println("Temperature in farenheit is :" + tempInFaren);
+
+		//Categorizing the temperature
+		if (tempInFaren < 45) {
+			System.out.println("COLD WEATHER");
+		} else if (tempInFaren > 85) {
+			System.out.println("PRETTY WARM");
+		} else {
+			System.out.println("NORMAL WEATHER");
+		}
+	}
+
+}
