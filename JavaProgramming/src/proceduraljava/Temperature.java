f863d3dbbae8bb258fc59880588273be68284206
ARUNA JAYANAND <aruna.k.jayanand@gmail.com>
2019-11-29 11:04:17 -0500

    Changed weather categorization logic

--- JavaProgramming/src/proceduraljava/Temperature.java
+++ JavaProgramming/src/proceduraljava/Temperature.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 
 /**
  * 
- * This class is to convert temperature in celcius to farenheit and categorize the wrather
- * as COLD WAETHER, PRETTY WARM or NORMAL.
+ * This class is to convert temperature in celcius to farenheit and categorize
+ * the wrather as COLD WAETHER, PRETTY WARM or NORMAL.
  * 
  * @author Aruna
  *
  */
 class Temperature {
-	
+
 	public static void main(String[] args) {
 
 		Scanner sc = new Scanner(System.in);
@@ -22,10 +22,10 @@ class Temperature {
 		float tempInFaren = tempCel * (9f / 5) + 32;
 		System.out.println("Temperature in farenheit is :" + tempInFaren);
 
-		//Categorizing the temperature
+		// Categorizing the temperature
 		if (tempInFaren < 45) {
 			System.out.println("COLD WEATHER");
-		} else if (tempInFaren > 85) {
+		} else if (tempInFaren > 80) {
 			System.out.println("PRETTY WARM");
 		} else {
 			System.out.println("NORMAL WEATHER");
